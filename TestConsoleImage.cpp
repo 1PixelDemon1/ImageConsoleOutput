465030ad210991ae3a32d0a9d28a11f45ca0c3bb
1PixelDemon1 <pxldem@gmail.com>
2023-04-18 22:29:40 +0500

    Added initial multithreading

--- TestConsoleImage.cpp
+++ TestConsoleImage.cpp
@@ -1,1 +1,19 @@
+#pragma once
+#define TEST_CONSOLE_IMAGE_
 #include "TestConsoleImage.h"
+
+void TestConsoleImage::getBMPImageInfo(std::string file) {
+	std::ifstream bmp(file, std::ios::binary);
+
+	byte_ header[54];
+	bmp.read(header, 54);
+
+	size.width = *(int*)(header + 18) - *(int*)(header + 18) % 2;
+	size.height = *(int*)(header + 22);
+	
+	mapSize = size.width * size.height * 3; //24 bytes
+	map = new byte_[mapSize];
+	bmp.read(map, mapSize);
+	
+	bmp.close();
+}
--- TestConsoleImage.h
+++ TestConsoleImage.h
@@ -1,16 +1,20 @@
 #pragma once
-
 typedef char byte_;
 
 #include <string>
 #include <fstream>
 #include <windows.h>
+#include <vector>
+#include <thread>
 
 
 class TestConsoleImage
 {
-
 private:
+	
+	size_t lineDelta;		// How many threads will be made.
+	size_t allowedTries;	// How many collision between threads shall we handle. 
+
 	byte_* map;
 	size_t mapSize;
 
@@ -21,42 +25,86 @@ private:
 	
 	HDC hdc;
 
-	void getBMPImageInfo(std::string file) {
-		std::ifstream bmp(file, std::ios::binary);
+	void getBMPImageInfo(std::string file);
 
-		byte_ header[54];
-		bmp.read(header, 54);
-
-		
-		size.width = *(int*)(header + 18);
-		size.height = *(int*)(header + 22);
-		mapSize = size.width * size.height * 3;
-		map = new byte_[mapSize];
-		bmp.read(map, mapSize);
-		bmp.close();
-	}
+#ifndef SINGLE_THREAD_CONSOLE_IMAGE // If programmer wants to use single-thread drawing.
+	void drawSegment(size_t line);
+	void drawSegments(size_t from, size_t to);
+#endif // SINGLE_THREAD_CONSOLE_IMAGE
 
 
 public:
-	TestConsoleImage() {
-		std::string file{"file.bmp"};
-		getBMPImageInfo(file);
-		hdc = GetDC(GetConsoleWindow());
 
+
+	TestConsoleImage(std::string filename, 
+		size_t lineDelta = 4, 
+		size_t allowedTries = 15) : lineDelta{ lineDelta }, hdc{GetDC(GetConsoleWindow())}, allowedTries{allowedTries} 
+	{		
+		getBMPImageInfo(filename);
 	}
 
-	void draw() {
-		int index{};
-		for (size_t i = 0; i < mapSize; i += 3)
-		{
-			SetPixel(hdc, index % size.width, size.height - index / (size.width), RGB((int) map[i + 2], (int) map[i + 1], (int) map[i]));
-			index++;
-		}
-
-	}
-
+	void draw();
+	
 	virtual ~TestConsoleImage() {
 		delete[] map;
 	}
 };
+
+
+
+#ifndef TEST_CONSOLE_IMAGE_ // To avoid cyclic include. 
+
+#ifdef SINGLE_THREAD_CONSOLE_IMAGE // If programmer wants to use single-thread drawing.
+
+void TestConsoleImage::draw() {
+
+	size_t span{};
+	for (size_t i = 0; i < size.height; i++)
+	{
+		for (size_t j = 0; j < size.width; j++)
+		{
+			SetPixel(hdc, j, size.height - i, RGB((int)map[span + j * 3 + 2], (int)map[span + j * 3 + 1], (int)map[span + j * 3]));
+		}
+		span += size.width * 3;
+	}
+
+}
+
+#else
+
+void TestConsoleImage::drawSegments(size_t from, size_t to) {
+	size_t span{ from * size.width * 3 }; // Previous lines.
+	size_t tries{};
+	for (size_t i = from; i < to; i++)
+	{
+		for (int32_t j = 0; j < size.width; j++)
+		{			
+			if (SetPixel(hdc, j, size.height - i, RGB((int)map[span + j * 3 + 2], (int)map[span + j * 3 + 1], (int)map[span + j * 3])) == -1) {
+				tries++;
+				if (tries < allowedTries) j--;
+				else tries = 0;
+			}
+		}
+		span += size.width * 3;
+	}
+}
+
+void TestConsoleImage::draw() {
+
+	std::vector<std::thread> threads;
+
+	size_t span{ size.height / lineDelta };
+	for (size_t i = 0; i < lineDelta; i++) {
+		threads.push_back(std::thread(&TestConsoleImage::drawSegments, this, i * span, i * span + span));
+	}
+
+	for (auto& thread : threads) {
+		thread.join();
+	}
+}
+
+#endif // SINGLE_THREAD_CONSOLE_IMAGE
+#endif // !TEST_CONSOLE_IMAGE_
+
+
 
